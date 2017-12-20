d2b7ac023b6528d8bcde26d9d4275896f0cd15fe
Huy <giahuy0000@yahoo.com.vn>
2017-12-21 01:10:02 +0700

    more

--- 11-Container With Most Water.cpp
+++ 11-Container With Most Water.cpp
@@ -5,22 +5,46 @@ using namespace std;
 
 class Solution {
 public:
-    int maxArea(vector<int>& height) {
+    int trap(vector<int>& height) {
         int n = height.size();
+
         int i = 0;
-        int j = n-1;
         int res = 0;
-        while (i < j) {
-            res = max(res, min(height[i], height[j])*(j-i));
-            if (height[i] < height[j]) i++; else j--;
+        while (i < n) {
+            int j = i + 1;
+            int sum = 0;
+            while (j < n && height[j] < height[i]) {
+                sum+= height[j];
+                j++;
+            }
+            if (j == n) break;
+            res+= (j-i-1)*height[i] - sum;
+            i = j;
         }
+
+        reverse(height.begin(), height.end());
+
+        i = 0;
+        while (i < n) {
+            int j = i + 1;
+            int sum = 0;
+            while (j < n && height[j] <= height[i]) {
+                sum+= height[j];
+                j++;
+            }
+            if (j == n) break;
+            res+= (j-i-1)*height[i] - sum;
+            i = j;
+        }
+
+
         return res;
     }
 };
 int main() {
     Solution* sol = new Solution();
-    vector<int> a = {9,2,6,1};
-    cout << sol->maxArea(a);
+    vector<int> a = {0,1,0,2,1,0,1,3,2,1,2,1};
+    cout << sol->trap(a);
 
 }
 
