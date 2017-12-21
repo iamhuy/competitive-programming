b6d11dd9901657280936b5817595e3719ef704a5
Huy <giahuy0000@yahoo.com.vn>
2017-12-21 15:31:59 +0700

    more

--- 15-3sum.cpp
+++ 15-3sum.cpp
@@ -8,31 +8,39 @@ class Solution {
 public:
     vector<vector<int> > threeSum(vector<int>& nums) {
         sort(nums.begin(), nums.end());
-        set<vector<int> > s;
+        vector<vector<int> > res;
         int n = nums.size();
 
         for (int k=0;k<n-2;k++) {
+            if (k > 0 && nums[k]==nums[k-1]) continue;
             int x = nums[k];
             int i = k+1;
             int j = n-1;
             while (i<j) {
                 if (nums[i]+nums[j]+x==0) {
+//                    cout << k << ' ' << i << ' ' << j << endl;
                     vector<int> candidate= {x,nums[i], nums[j]};
-                    if (s.find(candidate) == s.end()) s.insert(candidate);
-                    i++;
+                    res.push_back(candidate);
+                    while (i < j && nums[i] == nums[i+1]) i++;
+                    while (i < j && nums[j] == nums[j-1]) j--;
+                    i++;j--;
+//                    cout << i <<' ' <<  j << endl;
                 }
                 else if (nums[i]+nums[j]+x > 0) j--;
                 else if (nums[i]+nums[j]+x < 0) i++;
             }
         }
 
-        vector<vector<int> > res(s.begin(), s.end());
+
         return  res;
     }
 };
 
 
+
 int main() {
     Solution* sol = new Solution();
+    vector<int> x = {0,0,0,0};
+    sol->threeSum(x);
 }
 
